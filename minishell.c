bbf3e3ff6951ed03da5fe425c1c9809bcf33753e
BraisCabo <braiscabofelpete@gmail.com>
2022-11-19 11:02:23 +0100

    Multiple commands can be executd

--- minishell.c
+++ minishell.c
@@ -15,15 +15,13 @@ int main(void)
     FILE *fi, *fo, *fe;
     pid_t pid;
     int j = 0;
-    int p[2];
+    int p[2], p1[2];
     printf("msh> ");
     while (fgets(buffer, 1024, stdin))
     {
         line = tokenize(buffer);
-        if (line == NULL)
+        if (line->ncommands != 0)
         {
-            continue;
-        }
         pipe(p);
         pid = fork();
         if (pid == 0)
@@ -59,40 +57,86 @@ int main(void)
         else
         {
             close(p[1]);
-            j++;
-            pid = fork();
-            if (pid == 0)
+            for (j = 1; j < line->ncommands; j++)
             {
-                if (j == (line->ncommands - 1) && line->redirect_output != NULL)
+                if (j % 2 == 0)
                 {
-                    fo = fopen(line->redirect_output, "w");
-                    dup2(fileno(fo), STDOUT_FILENO);
-                    fclose(fo);
+                    pipe(p);
                 }
-                if (j == (line->ncommands - 1) && line->redirect_error != NULL)
+                else
                 {
-                    fe = fopen(line->redirect_error, "w");
-                    dup2(fileno(fe), STDERR_FILENO);
-                    fclose(fe);
+                    pipe(p1);
                 }
-                if (j > 0)
+                pid = fork();
+                if (pid == 0)
                 {
-                    dup2(p[0], STDIN_FILENO);
+                    if (j == (line->ncommands - 1) && line->redirect_output != NULL)
+                    {
+                        fo = fopen(line->redirect_output, "w");
+                        dup2(fileno(fo), STDOUT_FILENO);
+                        fclose(fo);
+                    }
+                    if (j == (line->ncommands - 1) && line->redirect_error != NULL)
+                    {
+                        fe = fopen(line->redirect_error, "w");
+                        dup2(fileno(fe), STDERR_FILENO);
+                        fclose(fe);
+                    }
+                    if (j % 2 == 0)
+                    {
+                    if (j > 0)
+                        {
+                            dup2(p1[0], STDIN_FILENO);
+                        }
+                        if (j < line->ncommands - 1)
+                        {
+                            dup2(p[1], STDOUT_FILENO);    
+                        }
+                    }
+                    else
+                    {
+                        if (j > 0)
+                        {
+                            dup2(p[0], STDIN_FILENO);
+                        }
+                        if (j < line->ncommands - 1)
+                        {
+                            dup2(p1[1], STDOUT_FILENO);
+                        }
+                    }
                     close(p[0]);
+                    close(p1[1]);
+                    close(p1[0]);
+                    close(p[1]);
+
+                    execvp(line->commands[j].argv[0], line->commands[j].argv);
+                    fprintf(stderr, "%s: No se encuentra el mandato\n", line->commands[j].argv[0]);
+                    exit(1);
                 }
-                execvp(line->commands[1].argv[0], line->commands[1].argv);
-                fprintf(stderr, "%s: No se encuentra el mandato\n", line->commands[j].argv[0]);
-                exit(1);
-            }
-            else
-            {
-                close(p[0]);
-                for (j = 0; j < line->ncommands; j++)
+                else
                 {
-                    wait(NULL);
+                    if (j % 2 == 0){
+                        dup2(p1[0], p[1]);
+                        close(p1[0]);
+                        close(p[1]);
+                        if (j == (line->ncommands - 1)){
+                            close(p[0]);
+                        }
+                    }else{
+                        dup2(p[0], p1[1]);
+                        close(p[0]);
+                        close(p1[1]);
+                        if (j == (line->ncommands - 1)){
+                            close(p1[0]);
+                        }
+                    }
                 }
-                printf("msh> ");
             }
-        }
+            for (j = 0; j < line->ncommands; j++)
+            {
+                wait(NULL);
+            }
+        }}
+        printf("msh> ");
     }
 }
